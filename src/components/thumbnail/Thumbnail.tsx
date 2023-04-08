dadcce12c664c19921f9da01c9cc2b1117be932c
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-04-08 15:02:07 +0500

    Row and slider

--- src/components/thumbnail/Thumbnail.tsx
+++ src/components/thumbnail/Thumbnail.tsx
@@ -2,13 +2,25 @@ import React from 'react'
 import { ThumbnailProps } from './Thumbnail.Props'
 import Image from 'next/image'
 import { image_base } from 'src/helpers/constants'
+import ReactStars from 'react-stars'
 
-function Thumbnail({movie}:ThumbnailProps) {
-  return (
-    <div className='relative h-[330px] min-w-[200px] cursor-pointer md:h-[440px] transition duration-200 md:hover:scale-110 ease-out md:min-w-[292px]'>
-         <Image src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`} alt={"movie.title"} fill className='object-cover rounded-sm md:rounded' />
-    </div>
-  )
+function Thumbnail({ movie, isBig = false }: ThumbnailProps) {
+    return (
+        <div className={`relative ${isBig ? 'h-[400px] md:h-[550px] min-w-[350px] md:min-w-[470px]' : 'h-[330px] md:h-[440px] min-w-[200px] md:min-w-[292px]'}   cursor-pointer  transition duration-200 md:hover:scale-110 ease-out `}>
+
+            <Image src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`} alt={"movie.title"} fill className='object-cover rounded-sm md:rounded' />
+                <div className='absolute left-0 bottom-0 right-0 top-0 w-full h-full bg-black/40'></div>
+
+            <div className='absolute bottom-5 left-4 right-2'>
+              
+                <div className='flex items-center space-x-2'>
+                    <ReactStars edit={false} count={10} value={movie?.vote_average} color2='white' />
+                    <p>({movie.vote_count})</p>
+                </div>
+                <h1 className='text-xl font-bold md:text-2xl '>{movie?.title || movie?.name || movie?.original_name}</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Thumbnail
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,13 @@ import { API_REQUEST } from 'src/services/Api.service'
 
 
 
-export default function Home({ trending, topRated }: HomeProps): JSX.Element {
+export default function Home({ trending, topRated, tvTopRated, popular, documentary, comedy, family, history }: HomeProps): JSX.Element {
 
 
 
 
   return (
-    <div className='relative h-[200vh]'>
+    <div className='relative min-h-screen'>
       <Head>
         <title>Home - Hojiakbar</title>
         <meta name="description" content="For watching movies you should sign to app" />
@@ -26,6 +26,11 @@ export default function Home({ trending, topRated }: HomeProps): JSX.Element {
         <Hero trending={trending} />
         <section>
           <Row title='Top Rated' movies={topRated}/>
+          <Row title='TV Show' movies={tvTopRated} isBig={true}/>
+          <Row title='Popular' movies={popular}/>
+          <Row title='Documentary' movies={documentary.reverse()}/>
+          <Row title='Comedy' movies={comedy}/>
+          <Row title='Family' movies={family.reverse()}/>
         </section>
       </main>
     </div>
@@ -36,10 +41,22 @@ export default function Home({ trending, topRated }: HomeProps): JSX.Element {
 export const getServerSideProps: GetServerSideProps = async () => {
   const trending = await fetch(API_REQUEST.trending).then(res => res.json())
   const topRated = await fetch(API_REQUEST.top_rated).then(res => res.json())
+  const tvTopRated = await fetch(API_REQUEST.tv_top_rated).then(res => res.json())
+  const popular = await fetch(API_REQUEST.popular).then(res => res.json())
+  const documentary = await fetch(API_REQUEST.documentary).then(res => res.json())
+  const comedy = await fetch(API_REQUEST.comedy).then(res => res.json())
+  const family = await fetch(API_REQUEST.family).then(res => res.json())
+  const history = await fetch(API_REQUEST.history).then(res => res.json())
   return {
     props: {
       trending: trending.results,
       topRated: topRated.results,
+      tvTopRated: tvTopRated.results,
+      popular: popular.results,
+      documentary: documentary.results,
+      comedy: comedy.results,
+      family: family.results,
+      history: history.results,
 
     }
   }
@@ -48,5 +65,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
 interface HomeProps {
   trending: IMovie[]
   topRated: IMovie[]
-
+  tvTopRated: IMovie[]
+  popular: IMovie[]
+  documentary: IMovie[]
+  comedy: IMovie[]
+  family: IMovie[]
+  history: IMovie[]
 }
